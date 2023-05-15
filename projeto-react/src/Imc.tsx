e8a32271441fb208969022631d5dc1aa470f5c99
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2023-05-14 22:43:23 -0300

    Initial commit

---
+++ projeto-react/src/Imc.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './Imc.css'
+
+function Imc() {
+  const [peso, setPeso] = useState(0)
+  const [altura, setAltura] = useState(0)
+  const [IMC, setIMC] = useState(0)
+
+  function calculaIMC(){
+    if ( !peso|| !altura)  return; 
+
+    const valorImc = peso / (altura * altura)
+    setIMC(valorImc.toFixed(2))
+  }
+
+  useEffect( () => {
+    calculaIMC()
+  }, [peso, altura])
+
+  return (
+    <>
+      <h1>Calculando IMC</h1>
+
+      <label>Peso</label>
+      <input name="peso" type="number" value={peso} min={0.1} step={0.1} onChange={(event) => setPeso(Number(event.target.value))}></input>  
+
+      <label>Altura</label>
+      <input name="altura" type="number" value={altura}  min={0.3} max={3} step={0.01} onChange={(event) => setAltura(Number(event.target.value))}></input>  
+
+      <button onClick={calculaIMC}>Calcular</button>
+
+      { IMC && 
+        <label>Seu IMC é {IMC}</label>
+      }
+
+    </>
+  )
+}
+
+export default Imc
